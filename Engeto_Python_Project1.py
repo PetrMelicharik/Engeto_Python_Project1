1693a9e0225f0cff506b19f5c4c1761280de06bc
PetrMelicharik <petr.melicharik@seznam.cz>
2024-02-08 20:15:15 +0100

    added texts

--- Engeto_Python_Project1.py
+++ Engeto_Python_Project1.py
@@ -6,6 +6,35 @@ heslo = input("heslo:")
 
 kontrola_hesla = uzivatele.get(jmeno)
 
+#texty
+texty = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
+]
+
 #přihlášení uživatele
 
 if jmeno in uzivatele and kontrola_hesla == heslo:
@@ -13,8 +42,8 @@ if jmeno in uzivatele and kontrola_hesla == heslo:
     print("Vítej v aplikaci,", jmeno)
     print("Máme zde 3 texty pro analýzu")
     print("-" * 30)
-    zvoleny_text = input("Zvol číslo textu od 1 do 3:")
-    print(zvoleny_text)
+    zvoleny_text = int(input("Zvol číslo textu od 1 do 3:"))
+    print(texty[zvoleny_text - 1])
 else:
     print("username:", jmeno)
     print("password:", heslo)
