94d839c65cf3115cfde1c4cd4cc177e3e9ac6b61
PetrMelicharik <petr.melicharik@seznam.cz>
2024-02-08 20:04:23 +0100

    user login

--- Engeto_Python_Project1.py
+++ Engeto_Python_Project1.py
@@ -1,20 +1,20 @@
 #proměnné
 uzivatele = {"bob": "123", "ann": "pass123", "mike": "password123", "liz": "pass123"}
 
-jmeno = input("Dobrý den, zadejte prosím uživatelské jméno:")
-heslo = input("Nyní zadejte heslo:")
+jmeno = input("uživatelské jméno:")
+heslo = input("heslo:")
 
 kontrola_hesla = uzivatele.get(jmeno)
 
 #přihlášení uživatele
 
 if jmeno in uzivatele and kontrola_hesla == heslo:
-    print("username:", jmeno)
-    print("password:", heslo)
     print("-" * 30)
-    print("Vítej v aplikaci", jmeno)
+    print("Vítej v aplikaci,", jmeno)
     print("Máme zde 3 texty pro analýzu")
     print("-" * 30)
+    zvoleny_text = input("Zvol číslo textu od 1 do 3:")
+    print(zvoleny_text)
 else:
     print("username:", jmeno)
     print("password:", heslo)
